e366a6095cebea62b87d651a2c306a3f206d4896
hyerin <hope1053@naver.com>
2022-03-13 15:48:09 +0900

    프로그래머스 OpenChatRoom

---
+++ Solutions/Programmers/String/OpenChatRoom.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func solution(_ recordList:[String]) -> [String] {
+	let ACTION = 0
+    let UID = 1 
+    let NICKNAME = 2
+	
+    var nicknameDict: [String: String] = [:]
+    
+    var answerList : [String] = []
+    
+    for recordSet in recordList {
+        let record = recordSet.components(separatedBy: " ")
+
+        if record[ACTION] == "Enter" || record[ACTION] == "Change" {
+            nicknameDict[record[UID]] = record[NICKNAME]
+        }
+    }
+    
+    for recordSet in recordList {
+        let record = recordSet.components(separatedBy: " ")
+        
+        let userName = nicknameDict[record[UID]] ?? ""
+        
+        if record[ACTION] == "Enter" {
+            answerList.append("\(userName)님이 들어왔습니다.")
+        } else if record[ACTION] == "Leave" {
+            answerList.append("\(userName)님이 나갔습니다.")
+        }
+    }
+    
+    return answerList
+    
+}
